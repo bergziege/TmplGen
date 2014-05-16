4ed158ef6aee92d365e300b25f44baab9b5f000f
Bernd Boruttau <berndnet2000@web.de>
2014-05-16 14:41:06 +0200

    Shell mit testweisen Fensterelementen

--- TmplGen/Ui/Shell/Shell.xaml.cs
+++ TmplGen/Ui/Shell/Shell.xaml.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
 
 namespace De.BerndNet2000.TmplGen.Ui.Shell {
     /// <summary>
-    /// Interaktionslogik für Shell.xaml
+    ///     Interaktionslogik für Shell.xaml
     /// </summary>
-    public partial class Shell : Window {
+    public partial class Shell : MetroWindow {
         public Shell() {
             InitializeComponent();
         }
